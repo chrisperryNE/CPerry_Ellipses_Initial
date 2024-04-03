6ed9e521b62f5cb703b328302d0324f7091e0614
chrisperryNE <perry.chris@northeastern.edu>
2024-04-03 23:19:33 +0000

    Add project files.

---
+++ CPerry_Ellipses/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CPerry_Ellipses
+{
+    
+    public partial class MainWindow : Window
+    {
+
+        Random r = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Ellipse newEllipse = new Ellipse
+            {
+                Width = 50,
+                Height = 50,
+                StrokeThickness = 3,
+                Stroke = Brushes.Black
+            };
+
+            Canvas.SetLeft(newEllipse, xslider.Value);
+            Canvas.SetTop(newEllipse, r.Next(25, 425));
+
+            canvas.Children.Add(newEllipse);
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+References:
+  https://www.youtube.com/watch?v=8eTWq27h4sY
+  https://wpf-tutorial.com/misc-controls/the-slider-control/#:~:text=Snapping%20to%20ticks,-If%20you%20have%20a%20look
